759db74a704759e7d2789ac638252082f40a51e4
Matthias Schmid <ramsondon@gmail.com>
2017-05-10 22:23:18 +0200

    rock n roll too big fat commit

---
+++ gulpfile.js
@@ -0,0 +1,99 @@
+var gulp = require('gulp');
+var less = require('gulp-less');
+var browserSync = require('browser-sync').create();
+var header = require('gulp-header');
+var cleanCSS = require('gulp-clean-css');
+var rename = require("gulp-rename");
+var concat = require("gulp-concat");
+var uglify = require('gulp-uglify');
+var filter = require('gulp-filter');
+var pkg = require('./package.json');
+
+// Set the banner content
+var banner = ['/*!\n',
+    ' * ramsondon.github.io - <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n',
+    ' * Copyright 2017 -' + (new Date()).getFullYear(), ' <%= pkg.author %>\n',
+    ' * Licensed under <%= pkg.license.type %> (<%= pkg.license.url %>)\n',
+    ' */\n',
+    ''
+].join('');
+
+// Compile LESS files from /less into /css
+gulp.task('less', function() {
+    var f = filter(['**', '!**/mixins.less', '!**/variables.less']);
+    return gulp.src('src/less/*.less')
+        .pipe(f)
+        .pipe(less().on('error', function(err) {
+			console.log(err);
+		}))
+        .pipe(header(banner, { pkg: pkg }))
+        .pipe(gulp.dest('src/css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+});
+
+// Minify compiled CSS
+gulp.task('minify-css', ['less'], function() {
+    return gulp.src('src/css/*.css')
+        .pipe(cleanCSS({ compatibility: 'ie8' }))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('web/css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+});
+
+// Minify JS
+gulp.task('minify-js', function() {
+    return gulp.src('src/js/**/*.js')
+        .pipe(uglify())
+        .pipe(header(banner, { pkg: pkg }))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('web/js'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+});
+
+// Copy vendor libraries from /node_modules into /vendor
+// gulp.task('copy', function() {
+    // gulp.src(['node_modules/bootstrap/dist/**/*', '!**/npm.js', '!**/bootstrap-theme.*', '!**/*.map'])
+    //     .pipe(gulp.dest('vendor/bootstrap'))
+	//
+    // gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
+    //     .pipe(gulp.dest('vendor/jquery'))
+	//
+    // gulp.src([
+    //         'node_modules/font-awesome/**',
+    //         '!node_modules/font-awesome/**/*.map',
+    //         '!node_modules/font-awesome/.npmignore',
+    //         '!node_modules/font-awesome/*.txt',
+    //         '!node_modules/font-awesome/*.md',
+    //         '!node_modules/font-awesome/*.json'
+    //     ])
+    //     .pipe(gulp.dest('vendor/font-awesome'))
+// })
+
+// Run everything
+gulp.task('default', ['less', 'minify-css', 'minify-js', 'copy']);
+
+// Configure the browserSync task
+gulp.task('browserSync', function() {
+    browserSync.init({
+        server: {
+            baseDir: ''
+        },
+    })
+})
+
+// Dev task with browserSync
+gulp.task('dev', ['browserSync', 'less', 'minify-css', 'minify-js'], function() {
+    gulp.watch('src/less/*.less', ['less']);
+    gulp.watch('src/css/*.css', ['minify-css']);
+    gulp.watch('src/js/*.js', ['minify-js']);
+    gulp.watch('web/data/posts.json');
+    // Reloads the browser whenever HTML or JS files change
+    gulp.watch('*.html', browserSync.reload);
+    gulp.watch('web/js/**/*.js', browserSync.reload);
+});
---
+++ src/js/post.js
@@ -0,0 +1,27 @@
+(function($) {
+	"use strict"; // Start of use strict
+	$.ajax({
+		url: 'web/data/posts.json',
+		method: 'get',
+		dataType: 'json',
+		complete: function(data) {
+			var tpl_post= $('#post-template');
+			var tpl_post_modal = $('#post-template-modal');
+
+			var posts = JSON.parse(data.responseText).posts;
+			var append_html = function (tpl, post) {
+				r = _.template(tpl.html());
+				html = r(model);
+				tpl.parent().append($(html));
+			};
+
+			var r, html, model;
+			for (var i=0; i < posts.length; i++) {
+				model = { model : posts[i] };
+				model.model.id = 'post-' + i;
+				append_html(tpl_post_modal, model);
+				append_html(tpl_post, model);
+			}
+		}
+	});
+})(jQuery); // End of use strict
