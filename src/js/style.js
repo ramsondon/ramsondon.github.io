3d5acaa03925e651fadde5aae052916dd5a4b966
Matthias Schmid <ramsondon@gmail.com>
2017-05-11 11:45:45 +0200

    implemented scroll top fade in and out

--- src/js/style.js
+++ src/js/style.js
@@ -2,15 +2,27 @@
 
 (function($) {
     "use strict"; // Start of use strict
+    var top_offset = 50;
 
     // jQuery for page scrolling feature - requires jQuery Easing plugin
     $('.page-scroll a').bind('click', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
-            scrollTop: ($($anchor.attr('href')).offset().top - 50)
+            scrollTop: ($($anchor.attr('href')).offset().top - top_offset)
         }, 1250, 'easeInOutExpo');
         event.preventDefault();
     });
+
+    var scrollTop = function() {
+		if ($(document).scrollTop   () > top_offset) {
+			$('.scroll-top').stop().fadeIn();
+		} else {
+			$('.scroll-top').stop().fadeOut();
+		}
+	};
+    $(document).bind('scroll', scrollTop);
+    $(window).bind('resize', scrollTop);
+    scrollTop();
 
     // Highlight the top nav as scrolling occurs
     $('body').scrollspy({
@@ -20,7 +32,7 @@
 
     // Closes the Responsive Menu on Menu Item Click
     $('.navbar-collapse ul li a').click(function(){ 
-            $('.navbar-toggle:visible').click();
+        $('.navbar-toggle:visible').click();
     });
 
     // Offset for Main Navigation
