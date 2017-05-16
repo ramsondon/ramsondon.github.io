bf957da62f90ed14df1cb939b9a4f4bfd3c5ac90
Matthias Schmid <ramsondon@gmail.com>
2017-05-16 20:07:33 +0200

    extracted templates for tech skills

--- src/js/post.js
+++ src/js/post.js
@@ -1,27 +1,41 @@
 (function($) {
 	"use strict"; // Start of use strict
-	$.ajax({
-		url: 'web/data/posts.json',
-		method: 'get',
-		dataType: 'json',
-		complete: function(data) {
-			var tpl_post= $('#post-template');
-			var tpl_post_modal = $('#post-template-modal');
 
-			var posts = JSON.parse(data.responseText).posts;
-			var append_html = function (tpl, post) {
-				r = _.template(tpl.html());
-				html = r(model);
-				tpl.after($(html));
-			};
-
-			var r, html, model;
-			for (var i=0; i < posts.length; i++) {
-				model = { model : posts[i] };
-				model.model.id = 'post-' + i;
-				append_html(tpl_post_modal, model);
-				append_html(tpl_post, model);
+	var _get = function(url, complete) {
+		$.ajax({
+			url: url,
+			method: 'get',
+			dataType: 'json',
+			complete: function(data) {
+				complete(JSON.parse(data.responseText).items)
 			}
+		});
+	};
+
+	var render = function (tpl, model) {
+		var r = _.template(tpl.html());
+		var html = r(model);
+		tpl.after($(html));
+	};
+
+	_get('web/data/posts.json', function(items) {
+		var tpl_post= $('#post-template');
+		var tpl_post_modal = $('#post-template-modal');
+
+		var model;
+		for (var i=0; i < items.length; i++) {
+			model = { model : items[i] };
+			model.model.id = 'post-' + i;
+			render(tpl_post_modal, model);
+			render(tpl_post, model);
 		}
 	});
+
+	_get('web/data/tech.json', function(items) {
+		var tpl_post= $('#skill-item-template');
+		for (var i=0; i < items.length; i++) {
+			render(tpl_post, { model : items[i] });
+		}
+	});
+
 })(jQuery); // End of use strict
