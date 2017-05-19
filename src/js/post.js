169fd2e7006834c243f89bfe7ac3d19e27968a12
Matthias Schmid <ramsondon@gmail.com>
2017-05-19 20:48:12 +0200

    refactored skill rendering

--- src/js/post.js
+++ src/js/post.js
@@ -34,14 +34,10 @@
 	_get('web/data/tech.json', function(items) {
 		var tpl_post= $('#skill-item-template');
 		items = items.shuffle();
-		var c = "visible-sm visible-md visible-lg";
-		var tmp = "";
 		for (var i=0; i < items.length; i++) {
 			render(tpl_post, { model : items[i] });
-
-			if ((i+1) % 3  == 0) {
-				tmp = ((i+1) % 6 == 0 ? c : "");
-				tpl_post.after('<div class="clearfix visible-xs '+ tmp +'"></div>');
+			if ((i+1) % 3  === 0) {
+				tpl_post.after('<div class="clearfix visible-xs '+ ((i+1) % 6 === 0 ? "visible-sm visible-md visible-lg" : "") +'"></div>');
 			}
 		}
 	});
