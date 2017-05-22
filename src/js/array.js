2c2f25cdb5f1e0f96a43fc86dae278a6c176e2ad
Matthias Schmid <ramsondon@gmail.com>
2017-05-22 09:53:28 +0200

    removed array.js

--- src/js/array.js
+++
@@ -1,5 +0,0 @@
-Array.prototype.shuffle = function() {
-	//  Chris Coyier, October 19, 2010 via https://css-tricks.com/snippets/javascript/shuffle-array/
-	for(var j, x, i = this.length; i; j = parseInt(Math.random() * i), x = this[--i], this[i] = this[j], this[j] = x);
-	return this;
-};
